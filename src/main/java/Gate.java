248cfedf762ee67bcf77d8bbe71f5e20b4a63161
Eduardo <d0801609@students.katyisd.org>
2021-11-11 08:08:24 -0600

    Added tile and gate Stuff, need to get role and graphic

---
+++ src/main/java/Gate.java
@@ -0,0 +1,26 @@
+public class Gate extends Tile
+{
+    //tile ID explenation- Gates-1-6 |treasureTile 7-14 (fire 7,8)(water 9,10)(wind 11,12)(earth 13,14)|Misalanous 15-24
+    //"Fools' Landing","Bronze Gate","Copper Gate","Gold Gate","Iron Gate","Silver Gate"
+
+    public Gate(int Tileints)
+    {
+        super(Tileints);// IN boardgame Class , must instiate and check for gate
+    }
+    public Role getRole()
+    {
+        if(getID()==1)
+        {return PILOT;}
+        if(getID()==2)
+        {return ENGINEER;}
+        if(getID()==3)
+        {return EXPLORER;}
+        if(getID()==4)
+        {return NAVIGATOR;}
+        if(getID()==5)
+        {return DIVER;}
+        if(getID()==6)
+        {return MESSENGER;}
+
+    }
+}
