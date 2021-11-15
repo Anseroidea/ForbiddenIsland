d34fbf2dddbf85cf6952bced33d346acdb7d6d14
irshadmir9 <spideeboy9@gmail.com>
2021-11-15 08:06:16 -0600

    card

--- src/main/java/InputScreen.java
+++ src/main/java/InputScreen.java
@@ -1,6 +1,17 @@
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+
 public class InputScreen {
     public Button submitButton;
     public Button addPlayerButton;
-
+    public Button removePlayerButton;
+    public HBox player1;
+    public HBox player2;
+    public VBox playerBox;
+    public ComboBox<String> difficulty;
+    public Label errorLabel;
 
 }
