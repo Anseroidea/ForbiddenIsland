9cc60e630e13cc11e6c21b2fa4261d8f6966a7ac
Anseroidea <K1525058@students.katyisd.rog>
2021-11-16 07:19:09 -0600

    stuff

--- src/main/java/player/TurnManager.java
+++ src/main/java/player/TurnManager.java
@@ -22,31 +22,41 @@ public class TurnManager {
         return actions;
     }
 
-    public static void endTurn(){
+    public static void endTurn() {
 
     }
 
     public static boolean addAction(String s){
         if (actions < 3){
             String lastAction = actionStrings.peek();
-            if (lastAction.startsWith("N") && ){
+            String player = lastAction.substring(1);
+            String coord1 = lastAction.substring(lastAction.indexOf("("), lastAction.indexOf("(") + 6);
+            String coord2 = lastAction.substring(lastAction.indexOf(")"), lastAction.indexOf("(") + 6);
+            if (lastAction.startsWith("N") && isTwoSpaces(coord1, coord2){
+                actionStrings.pop();
 
             } else if (lastAction.startsWith("E"))
         }
     }
 
 
-
-    private boolean isTwoSpaces(String coord1, String coord2){
-        String[] coords1 = coord1.replace("(", "").replace(")", "").split(",");
+    /**
+     * @param coord1 initial coordinate of the moved player ex: (1, 4)
+     * @param coord2 final coordinate of the moved player ex: (1, 4)
+     * @return true/false whether those coordinates are within two spaces of each other
+     */
+    private static boolean isTwoSpaces(String coord1, String coord2){
+        String[] coords1 = coord1.replace("(", "").replace(")", "").split(", ");
+        String[] coords2 = coord2.replace("(", "").replace(")", "").split(", ");
+        return isTwoSpaces(coords1[0], coords1[1], coords2[0], coords2[1]);
     }
 
-    private boolean isTwoSpaces(String x1, String x2, String y1, String y2){
+    private static boolean isTwoSpaces(String x1, String x2, String y1, String y2){
         return isTwoSpaces(Integer.parseInt(x1), Integer.parseInt(x2), Integer.parseInt(y1), Integer.parseInt(y2));
     }
 
-    private boolean isTwoSpaces(int x1, int x2, int y1, int y2){
-        return (Math.abs(x1 - x2) + Math.abs(y1 - y2)) > 1;
+    private static boolean isTwoSpaces(int x1, int x2, int y1, int y2){
+        return (Math.abs(x1 - x2) + Math.abs(y1 - y2)) <= 2;
     }
 
     public static List<String> toFormattedStrings(){
