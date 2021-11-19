11c677776a77414e0d68ac99247bb9e5d8215026
Anseroidea <K1525058@students.katyisd.rog>
2021-11-19 08:05:42 -0600

    stuff

--- src/main/java/graphics/InputScreen.java
+++ src/main/java/graphics/InputScreen.java
@@ -1,19 +1,52 @@
 package graphics;
 
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class InputScreen {
-    public Button submitButton;
-    public Button addPlayerButton;
-    public Button removePlayerButton;
-    public HBox player1;
-    public HBox player2;
-    public VBox playerBox;
-    public ComboBox<String> difficulty;
-    public Label errorLabel;
+    public RadioButton fourButton;
+    public RadioButton twoButton;
+    public RadioButton threeButton;
+    public RadioButton noviceButton;
+    public RadioButton normalButton;
+    public RadioButton eliteButton;
+    public RadioButton legendaryButton;
+    public List<RadioButton> difficultyButtons = Arrays.asList(twoButton, threeButton, fourButton);
+    public List<RadioButton> numPlayersButtons = Arrays.asList(noviceButton, normalButton, eliteButton, legendaryButton);
+    private int difficulty;
+    private int numPlayers;
 
+    public void submit(){
+        if (difficultyButtons.stream().filter(ToggleButton::isSelected).count() == 1 && numPlayersButtons.stream().filter(ToggleButton::isSelected).count() == 1){
+            if (noviceButton.isSelected()){
+                difficulty = 20;
+            } else if (normalButton.isSelected()){
+                difficulty = 21;
+            } else if (eliteButton.isSelected()) {
+                difficulty = 30;
+            } else {
+                difficulty = 31;
+            }
+            if (twoButton.isSelected()){
+                numPlayers = 2;
+            } else if (threeButton.isSelected()){
+                numPlayers = 3;
+            } else {
+                numPlayers = 4;
+            }
+        } else if (difficultyButtons.stream().filter(ToggleButton::isSelected).count() == 1){
+
+        }
+    }
+
+    public void showErrorText(String s){
+
+    }
 }
