943bc1182b00a6296cc8ffb4a1f07e768fce88b8
irshadmir9 <spideeboy9@gmail.com>
2021-11-28 17:41:39 -0600

    kjqwerhgyqeuvbraeuob

--- src/main/java/card/FloodCard.java
+++ src/main/java/card/FloodCard.java
@@ -3,9 +3,14 @@ package card;
 import board.Tile;
 import card.Card;
 
+import java.awt.image.BufferedImage;
+
 public class FloodCard extends Card {
     private Tile tile;
 
+    public FloodCard(String n, BufferedImage b){
+        super(n, b);
+    }
     public Tile getTile(){
         return tile;
     }
